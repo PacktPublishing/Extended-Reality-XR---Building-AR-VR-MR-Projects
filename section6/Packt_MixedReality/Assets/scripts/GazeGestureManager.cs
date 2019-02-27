2798edb8dded7a48746da6c5208b7ca4214f7abd
Parul Bansal <pbansal7@sapient.com>
2019-02-28 01:52:22 +0530

    initial commit for section 6 project

---
+++ section6/Packt_MixedReality/Assets/scripts/GazeGestureManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour {
+
+    public static GazeGestureManager GazeInstance { get; private set; }
+    public static GameObject FocusedObject { get; private set; }
+    GestureRecognizer recognizer;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        GazeInstance = this;
+
+        recognizer = new GestureRecognizer();
+        recognizer.Tapped += (args) =>
+        {
+            if(FocusedObject != null)
+            {
+                Debug.Log("Tap Gesture using Space Bar");
+
+                animator = FocusedObject.GetComponentInParent<Animator>();
+
+                if(animator.speed == 0)
+                {
+                    animator.speed = 0.7f;
+                } else
+                {
+                    animator.speed = 0.0f;
+                }
+            }
+        };
+        recognizer.StartCapturingGestures();
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        GameObject oldObject = FocusedObject;
+
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            FocusedObject = hitInfo.collider.gameObject;
+        } else
+        {
+            FocusedObject = null;
+        }
+
+        if (FocusedObject != oldObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+	}
+}
---
+++ section6/Packt_MixedReality/Assets/scripts/GazeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeManager : MonoBehaviour {
+
+    private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            meshRenderer.enabled = true;
+
+            this.transform.position = hitInfo.point;
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        } else
+        {
+            meshRenderer.enabled = false;
+        }
+	}
+}
