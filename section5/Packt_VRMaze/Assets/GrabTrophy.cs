886d18fd301b582839df385c79c719944ec771d5
tst-iiota-mayjain <tst-iiota-mayjain@global.publicisgroupe.net>
2019-02-28 02:27:29 +0530

    initial commit

---
+++ section5/Packt_VRMaze/Assets/GrabTrophy.cs
@@ -0,0 +1,56 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class GrabTrophy : VRTK_InteractableObject
+    {
+        public AudioClip otherClip;
+        private VRTK_ControllerReference controllerReference;
+        public  AudioSource audioSource;
+        public AudioSource emulatorAudioSource;
+
+        public override void Grabbed(VRTK_InteractGrab grabbingObject)
+        {
+            base.Grabbed(grabbingObject);
+            controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+            if (VRTK_ControllerReference.IsValid(controllerReference) && IsGrabbed())
+            {
+                VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, 0.5f, 0.5f, 0.01f);
+                //strength, duration, pulse interval
+                PlayAudio();
+                //Glorious_Triumph
+            }
+
+
+        }
+
+        public void PlayAudio()
+        {
+            if (audioSource.enabled == true)
+            {
+                audioSource.clip = otherClip;
+
+                if (audioSource != null && !audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+            }
+
+            if (emulatorAudioSource.enabled == true)
+                {
+                    emulatorAudioSource.clip = otherClip;
+
+                    if (emulatorAudioSource != null && !emulatorAudioSource.isPlaying)
+                    {
+                        emulatorAudioSource.Play();
+                    }
+                }
+        }
+
+        public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+        {
+            base.Ungrabbed(previousGrabbingObject);
+            controllerReference = null;
+        }
+    }
+}
---
+++ section5/Packt_VRMaze/Assets/WalkThroughMaze.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkThroughMaze : MonoBehaviour
+{
+    public Transform targetTransform;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(targetTransform.position);
+    }
+}
