de6553844bd3769eb8c02045e664f42257bd4635
Parul Bansal <pbansal7@sapient.com>
2019-02-28 02:47:00 +0530

    initial commit

---
+++ section4/section4.4_Gaze_Castle/Assets/Scripts/Follower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += mainCamera.transform.forward
+                                * speed * Time.deltaTime;
+        
+    }
+}
---
+++ section4/section_4.3_solar_system/Assets/Scripts/RotateAroundPlanet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPlanet : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject objectToRotateAround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(
+            objectToRotateAround.transform.position,
+            Vector3.up,
+            speed * Time.deltaTime);
+
+    }
+}
